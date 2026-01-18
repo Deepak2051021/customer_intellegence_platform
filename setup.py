f4102f48257b446ed46e5379951f5cb808a83cf8
Deepak Gadala <deepak2051021gadala@gmail.com>
2026-01-18 15:19:10 +0530

    data training and preprocessing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'customer_intellegence',
+    version= '0.0.0',
+    author= 'deepak',
+    author_email= 'deepak2051021gadala@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
